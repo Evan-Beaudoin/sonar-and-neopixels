98fc2245b49a7ff7d6e8efd15f7475aea23e2a94
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-02 13:19:53 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let distance2 = 0
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    distance2 = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    if (distance2 > 6) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (distance2 < 6 || distance2 == 6) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (distance2 < 3 || distance2 == 3) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    basic.pause(200)
 })
